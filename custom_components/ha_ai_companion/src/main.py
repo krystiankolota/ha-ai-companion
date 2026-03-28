e494a334da0069534a64743d1b63e00b8362df82
krystian.kolota <krystiankolota@gmail.com>
2026-03-28 20:59:29 +0100

    test 1.1.3

--- custom_components/ha_ai_companion/src/main.py
+++ custom_components/ha_ai_companion/src/main.py
@@ -15,7 +15,7 @@ from .agents import AgentSystem
 from .memory import MemoryManager
 from .conversations import ConversationManager
 
-version = "1.1.2"
+version = "1.1.3"
 
 # Configure logging
 log_level = os.getenv('LOG_LEVEL', 'info').upper()
